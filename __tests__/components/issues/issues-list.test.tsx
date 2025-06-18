818f8be3838bc7fbbc786af4c22b13b36d73c7db
Michael Bastos <bastosmichael@gmail.com>
2025-06-18 08:34:30 -0500

    Mock router in issues list tests

--- __tests__/components/issues/issues-list.test.tsx
+++ __tests__/components/issues/issues-list.test.tsx
@@ -7,6 +7,9 @@ jest.mock("../../../db/queries/issues-queries", () => ({
 }))
 import { deleteIssue, updateIssuesFromGitHub } from "../../../db/queries/issues-queries"
 
+const router = { refresh: jest.fn() }
+jest.mock("next/navigation", () => ({ useRouter: () => router }))
+
 describe("IssuesList", () => {
   beforeEach(() => jest.clearAllMocks())
 
