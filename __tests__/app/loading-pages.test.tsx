8701f2dfea885b4aae5b13769ddc97e4665e70a6
Michael Bastos <bastosmichael@gmail.com>
2025-06-20 15:51:56 -0500

    Fix test imports for loading pages

--- __tests__/app/loading-pages.test.tsx
+++ __tests__/app/loading-pages.test.tsx
@@ -1,11 +1,11 @@
 import React from 'react'
 import { render } from '@testing-library/react'
 
-import InstructionsLoadingPage from '../app/[workspaceId]/[projectId]/instructions/loading'
-import IntegrationsLoadingPage from '../app/[workspaceId]/[projectId]/integrations/loading'
-import IssuesLoadingPage from '../app/[workspaceId]/[projectId]/issues/loading'
-import TemplatesLoadingPage from '../app/[workspaceId]/[projectId]/templates/loading'
-import SettingsLoadingPage from '../app/[workspaceId]/[projectId]/settings/loading'
+import InstructionsLoadingPage from '../../app/[workspaceId]/[projectId]/instructions/loading'
+import IntegrationsLoadingPage from '../../app/[workspaceId]/[projectId]/integrations/loading'
+import IssuesLoadingPage from '../../app/[workspaceId]/[projectId]/issues/loading'
+import TemplatesLoadingPage from '../../app/[workspaceId]/[projectId]/templates/loading'
+import SettingsLoadingPage from '../../app/[workspaceId]/[projectId]/settings/loading'
 
 const pages = [
   InstructionsLoadingPage,
